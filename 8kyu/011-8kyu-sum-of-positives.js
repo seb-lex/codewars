a5d740a1f36e41fa0a73a98b68543f77462fd6b8
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-08-18 23:49:04 +0800

    takes array and returns sum of positive nums

---
+++ 8kyu/011-8kyu-sum-of-positives.js
@@ -0,0 +1,10 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+function positiveSum(arr) {
+    let sum = 0;
+     for (let i = 0; i < arr.length; i++) {
+         if (arr[i] > 0) {
+         sum += arr[i];
+         }
+     }
+     return sum;
+}
