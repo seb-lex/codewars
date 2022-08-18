5c5ca6eddaf9020f033fae95c5c450a007b71773
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-08-18 23:41:48 +0800

    find out why this does not multiply

---
+++ 8kyu/001-8kyu-multiply.js
@@ -0,0 +1,15 @@
+// This code does not execute properly. Try to figure out why.
+//
+// function multiply(a, b){
+//     a * b
+//   }
+
+// Solution
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(1,1)); // 1)
+console.log(multiply(2,1)); // 2);
+console.log(multiply(2,2)); // 4);
+console.log(multiply(3,5)); // 15)
